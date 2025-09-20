44cdf7f25777ef8a87d6769cf708c924fbf7e795
brunorsnts <santos_2002@outlook.com.br>
2025-09-20 16:37:18 -0300

    Order, Instant, ISO 8601 - Part 2

---
+++ src/main/java/com/santos/teste/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.santos.teste.services;
+
+import com.santos.teste.entities.Order;
+import com.santos.teste.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> findAll() {
+        return repository.findAll();
+    }
+
+    public Order findById(Long id) {
+        Optional<Order> obj = repository.findById(id);
+        return obj.get();
+    }
+}
