80d4767e44afcf4ba868c129fcc43c362ba69efe
brunorsnts <santos_2002@outlook.com.br>
2025-09-25 13:38:19 -0300

    Exception handling - delete

--- src/main/java/com/santos/teste/services/UserService.java
+++ src/main/java/com/santos/teste/services/UserService.java
@@ -2,8 +2,11 @@ package com.santos.teste.services;
 
 import com.santos.teste.entities.User;
 import com.santos.teste.repositories.UserRepository;
+import com.santos.teste.resourses.exceptions.DatabaseException;
 import com.santos.teste.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+           throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
